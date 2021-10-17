3c7ac77ddcba102299406e42ecdfef81a6d707c9
adars <adars@host.docker.internal>
2021-10-17 18:00:29 +0530

    this is my batch 16 test class

---
+++ batch14/src/batch14/Batch16Test.java
@@ -0,0 +1,10 @@
+package batch14;
+
+public class Batch16Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is batch 16.....");
+	}
+
+}
